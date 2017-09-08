743607c968270278f4fe8a85f84c8a6e29165992
rasca0027 <rasca0027@gmail.com>
2017-09-07 23:33:05 -0400

    About page style

--- app/components/About.js
+++ app/components/About.js
@@ -8,6 +8,7 @@ import Row from 'muicss/lib/react/row';
 import Col from 'muicss/lib/react/col';
 import { Link } from 'react-router';
 import styles from '../styles.css';
+import heroImg from '../images/hero_big.jpg';
 
 import AboutData from '../data/AboutData';
 
@@ -26,39 +27,49 @@ export default class About extends React.PureComponent {
   render() {
     const lang = this.props.lang || "en";
     const { tab } = this.state;
-    const org = AboutData.organizations[tab];
-    const text =
-      <Col md="10" md-offset="1">
-        {org.introduction[lang]}
-        <h3 className={styles.heading}> MISSION </h3>
-        <div className={styles.headingBottom} />
-        {org.mission[lang]}
-      </Col>;
 
     return (
       <div>
-        <div className={styles.aboutImgContainer}>
-          <img src={AboutData.aboutImageSource} alt={AboutData.aboutImageAlternativeText[lang]} className={styles.aboutImg}/>
+        <div className={styles.aboutHero}>
+          <Container className={styles.heroCopy}>
+            <Col md="10" md-offset="1">
+              <h1>About the Movement</h1>
+              <p>{AboutData.banner}</p>
+            </Col>
+          </Container>
         </div>
-        <div className={styles.tabBand}>
-          {AboutData.organizations.map((org, index) =>
-            <div key={index} className={tab === index
-              ? (index === 0 ? styles.tabCategoryLeftActive : styles.tabCategoryActive)
-              : (index === 0 ? styles.tabCategoryLeft : styles.tabCategory)} onClick={() => this.tabClick(index)}>
-              {org.name[lang]}
-            </div>
-          )}
+        <div className={styles.sectionBodyWhite}> 
+          <Container className={styles.homeBody}>
+            <Row>
+              <Col md="8">
+                <h3 className={styles.homeHeading}> WHO WE ARE</h3>
+                <p>{AboutData.whoweare}</p>
+              </Col>
+              <Col md="4">
+                <h3 className={styles.homeHeading}> WHAT WE DO</h3>
+                <ul>
+                  <li>Advocacy</li>
+                  <li>Outreach</li>
+                  <li>Relationship-building</li>
+                </ul>
+              </Col>
+            </Row>
+          </Container>
         </div>
-        <Container className={styles.landingBody}>
-          <Row>
-            <img src={org.logoImageSource} alt={org.logoImageAlternativeText[lang]} className={styles.hostLogo} height="114px" />
-            {text}
-          </Row>
-        </Container>
-        <div style={{height: 150, width: '100%', backgroundColor: '#64a680'}}>
-          <Link to="register">
-            <Button color="primary" className={styles.applyNowButton}>Register Now</Button>
-          </Link>
+        <div className={styles.sectionBodyBlack}> 
+          <Container className={styles.homeBody}>
+            <Row>
+              <Col md="6">
+                <h3 className={styles.homeHeadingBlack}> History</h3>
+                <p>{AboutData.history[0]}</p>
+                <p>{AboutData.history[1]}</p>
+              </Col>
+              <Col md="6">
+                <img src={AboutData.historyImg[0]} className={styles.historyImg} />
+                <img src={AboutData.historyImg[1]} className={styles.historyImg} />
+              </Col>
+            </Row>
+          </Container>
         </div>
       </div>
     );
